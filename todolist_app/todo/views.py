f8412dd39e8a5f90d75539b92adb269d867c2d2c
jun <jun.kyoseikan@icloud.com>
2025-04-18 21:28:01 +0900

    code1

--- todolist_app/todo/views.py
+++ todolist_app/todo/views.py
@@ -7,29 +7,29 @@ def todo_list(request):
     todos = ToDoItem.objects.all()
     print(todos.last().title)
     print(todos.last().description)
-    return render(request, 'todo/todo_list.html', {'todos': [{"title": "タイトル", "description": "説明"}]})
+    return render(request, 'todo/todo_list.html', {'todos': [{"title": "", "description": ""}]})
 
 #リストの追加
 def add_todo(request):
     print(request.method)
-    if request.method == 'POST': #POSTメソッドであるかどうかを判断
-        print("POSTメソッドです")
-        title = request.POST.get('title') #titleを取得
-        print(title)
-        description = request.POST.get('description') #descriptionを取得
-        print(description)
-        todo_item = ToDoItem(title="テストタイトル", description="テスト説明") #ToDoItemを作成
-        title == 'POST' #POSTメソッドであるかどうかを判断
-        todo_item.save() #保存
-        return redirect('/') #リストを追加
+    if request.method == 'POST':
+        print('post')
+        title = request.POST.get('title')
+        description = request.POST.get('description')
 
-    print("GETメソッドです")
+        if title and description:
+            print('ok')
+            todo_item = ToDoItem(title=title, description=description)
+            todo_item.save()
+        return render(request, 'todo/add_todo.html')
+        
+    print('not')
     return render(request, 'todo/add_todo.html')
 
 #リストの編集
 def edit_todo(request, todo_id):
-    todo_item = ToDoItem.objects.get(id=todo_id) #idを取得
-    return render(request, 'todo/edit_todo.html', {'todo_item': todo_item}) #リストを編集する
+    response = "You're edting the todo item %s."
+    return render(request, 'todo/edit_todo.html', {'todo_item'}) 
 
 #リストの削除
 def delete_todo(request, todo_id):
@@ -39,4 +39,5 @@ def delete_todo(request, todo_id):
 
 #リストの表示
 def index(request):
+    print(ToDoItem.objects.all())
     return render(request, 'todo/index.html')
